4925f846e5414e5fa344c5cc4540fc182f8d6bc4
Vasily Kondratyev <wassily.kondratiev@gmail.com>
2022-09-26 18:30:02 +0300

    Randomization

--- src/testcases/gamma.rs
+++ src/testcases/gamma.rs
@@ -2,6 +2,7 @@
 
 use super::Test;
 use crate::error::Result;
+use std::{thread, time::Duration};
 
 pub fn get() -> Box<dyn Test + Send> {
     Box::new(Object {})
@@ -17,7 +18,12 @@ impl Test for Object {
     fn setup(&self) {}
 
     fn run(&self) -> Result<bool> {
-        println!("gamma");
+        use rand::Rng as _;
+        println!("enter gamma");
+        let mut rng = rand::thread_rng();
+        let delay = Duration::from_millis(rng.gen_range(1..=1000));
+        thread::sleep(delay);
+        println!("leave gamma");
         Ok(true)
     }
 
